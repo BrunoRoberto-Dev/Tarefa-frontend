fe160f748ab8abe3f263a72e4d1fe1c62e8ddada
Bruno Lima <bruno.robertinho06@gmail.com>
2021-10-26 21:28:50 -0300

    Adicionando variáveis de ambiente

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,14 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import * as dotenv from 'dotenv';
+ 
+dotenv.config();
  
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
-);
+  );
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,10 @@
-
-import axios from 'axios'
+import axios from 'axios';
+import dotenv from 'dotenv';
+ 
+dotenv.config();
  
 const api = axios.create({
-    baseURL: 'https://tarefabackend.herokuapp.com'
+    baseURL: process.env.REACT_APP_BACKEND
 })
  
 export default api;
